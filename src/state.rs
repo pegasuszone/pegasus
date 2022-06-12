994fa192255fb044a7c0f386d5d0e3cb3c4cae06
Maurits Bos <mb.brains@gmail.com>
2022-06-12 13:17:06 -0500

    files generated by cargo generate

---
+++ src/state.rs
@@ -0,0 +1,16 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::Addr;
+use cw_storage_plus::Item;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct State {
+    pub owner: Addr,
+    pub requested_token_id: String,
+    pub peer: Option<Addr>,
+}
+
+
+
+pub const STATE: Item<State> = Item::new("state");
