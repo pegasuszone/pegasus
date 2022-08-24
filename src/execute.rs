3d09d3802f680107a68e3e10bdd33e1dba8c9d65
Maurits Bos <mb.brains@gmail.com>
2022-08-24 14:38:57 +0200

    Added execute_accept_offer

--- src/execute.rs
+++ src/execute.rs
@@ -6,9 +6,9 @@ use crate::query::query_offers_by_sender;
 
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{Addr, DepsMut, Env, MessageInfo, Timestamp, Deps};
+use cosmwasm_std::{Addr, DepsMut, Env, MessageInfo, Timestamp, Deps, WasmMsg, to_binary, SubMsg};
 use cw2::set_contract_version;
-use cw721::{OwnerOfResponse};
+use cw721::{OwnerOfResponse, Cw721ExecuteMsg};
 use cw721_base::helpers::Cw721Contract;
 use sg_std::Response;
 
@@ -58,7 +58,7 @@ pub fn execute(
             expires_at),
             
         ExecuteMsg::RemoveOffer { id } => execute_remove_offer(deps, info, id),
-        ExecuteMsg::AcceptOffer { id } => execute_accept_offer(deps, id),
+        ExecuteMsg::AcceptOffer { id } => execute_accept_offer(deps, env, info, id),
         ExecuteMsg::RejectOffer { id } => execute_reject_offer(deps, info, id),
         ExecuteMsg::RemoveStaleOffer { id } => execute_remove_stale_offer(deps, env, info, id),
     }
@@ -130,8 +130,70 @@ fn execute_remove_offer(deps:DepsMut,info: MessageInfo, id:u8) -> Result<Respons
     )
 }
 
-fn execute_accept_offer(_deps:DepsMut, _id:u8) -> Result<Response, ContractError> {
-    todo!()
+fn execute_accept_offer(deps:DepsMut, env: Env, info: MessageInfo, id:u8) -> Result<Response, ContractError> {
+    let offer = offers().load(deps.storage, &[id])?;
+
+    let params = SUDO_PARAMS.load(deps.storage)?;
+
+
+    // check if the sender is the peer of the offer
+    if offer.peer != info.sender {
+        return Err(ContractError::UnauthorizedOperator {  })
+    }
+
+    // check if the offer is not yet expired
+    params.offer_expiry.is_valid(&env.block, offer.expires_at)?;
+
+    // check if the sender owns the requested nfts
+    for token in offer.wanted_nfts.clone() {
+        only_owner(deps.as_ref(), &info, &token.collection, token.token_id)?;
+
+        // check if the contract is approved to send transfer the tokens
+        Cw721Contract(token.collection.clone()).approval(
+            &deps.querier,
+            token.token_id.to_string(),
+            env.contract.address.to_string(),
+            None,
+        )?;
+    }
+
+    // check if the offeror owns the offered nfts
+    for token in offer.offered_nfts.clone() {
+        if offer.sender != Cw721Contract(token.collection.clone()).owner_of(&deps.querier, token.token_id.to_string(), false)?.owner {
+            return Err(ContractError::UnauthorizedPeer { collection: token.collection.into_string(), token_id: token.token_id, peer: offer.sender.to_string() })
+        }
+
+        // check if the contract is approved to send transfer the tokens
+        Cw721Contract(token.collection.clone()).approval(
+            &deps.querier,
+            token.token_id.to_string(),
+            env.contract.address.to_string(),
+            None,
+        )?;
+    }
+    let mut res = Response::new();
+    
+    // remove the offer
+    offers().remove(deps.storage, &[offer.id])?;
+
+    // transfer nfts
+    transfer_nfts(offer.peer.to_string(), offer.offered_nfts.clone(), &mut res)?;
+    transfer_nfts(offer.sender.to_string(), offer.wanted_nfts.clone(), &mut res)?;
+
+
+    Ok(res
+        .add_attribute("action", "accept_offer")
+    )
+}
+
+fn transfer_nfts(recipient: String, nfts: Vec<Token>, res: &mut cosmwasm_std::Response<sg_std::StargazeMsgWrapper>) -> Result<(), ContractError> {
+    Ok(for token in nfts {
+        let cw721_transfer_msg = Cw721ExecuteMsg::TransferNft { recipient: recipient.clone(), token_id: token.token_id.to_string() };
+        let exec_cw721_transfer_msg = WasmMsg::Execute { contract_addr: token.collection.to_string(), msg: to_binary(&cw721_transfer_msg)?, funds: vec![] };
+    
+        res.messages
+            .push(SubMsg::new(exec_cw721_transfer_msg));
+    })
 }
 
 fn execute_reject_offer(deps:DepsMut, info: MessageInfo, id:u8) -> Result<Response, ContractError> {
