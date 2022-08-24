0520b8ba014d29bc2a08b651c4d6e2dee8e4828d
Josef Leventon <josef.leventon@gmail.com>
2022-08-24 09:52:15 -0400

    Remove unused error

--- src/error.rs
+++ src/error.rs
@@ -10,18 +10,22 @@ pub enum ContractError {
 
     #[error("UnauthorizedOwner")]
     UnauthorizedOwner {},
-    
     #[error("Token (collection: {collection:?}, id: {token_id:?}) is already offered in offer {offer_id:?}" )]
-    TokenAlreadyOffered {collection: String, token_id: u32, offer_id:u8},
-    
-    #[error("Peer {peer:?} is not owner of Token (collection: {collection:?}, id: {token_id:?})" )]
-    UnauthorizedPeer {collection: String, token_id: u32, peer: String},
+    TokenAlreadyOffered {
+        collection: String,
+        token_id: u32,
+        offer_id: u8,
+    },
+
+    #[error("Peer {peer:?} is not owner of Token (collection: {collection:?}, id: {token_id:?})")]
+    UnauthorizedPeer {
+        collection: String,
+        token_id: u32,
+        peer: String,
+    },
 
     #[error("UnauthorizedOperator")]
     UnauthorizedOperator {},
-
-    #[error("Invalid finders fee bps: {0}")]
-    InvalidFindersFeeBps(u64),
 
     #[error("{0}")]
     ExpiryRange(#[from] ExpiryRangeError),
