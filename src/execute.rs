11287c170f5c77611beb216437d99a26302b9635
Maurits Bos <mb.brains@gmail.com>
2022-09-17 17:11:42 +0200

    reordered contract execute checks

--- src/execute.rs
+++ src/execute.rs
@@ -33,6 +33,13 @@ pub fn execute_create_offer(
     let offers_from_sender = query_offers_by_sender(deps.as_ref(), info.sender.clone())?;
     let params = SUDO_PARAMS.load(deps.storage)?;
 
+    // check if the expiry date is valid
+    let expires =
+        expires_at.unwrap_or_else(|| env.block.time.plus_seconds(params.offer_expiry.min + 1));
+    params
+        .offer_expiry
+        .is_valid(&env.block, env.block.time, expires)?;
+
     let api = deps.api;
 
     // Return an error if the amount of offers by this user + 1 exceeds the limit of active offers
@@ -56,9 +63,32 @@ pub fn execute_create_offer(
     let mut offered_nfts: Vec<Token> = vec![];
     let mut wanted_nfts: Vec<Token> = vec![];
 
+    // check if the peer is the owner of the requested tokens
+    for token in wanted_tokens {
+        // Verify token collection addr
+        let collection = api.addr_validate(&token.collection)?;
+
+        let token = Token {
+            collection,
+            token_id: token.token_id,
+        };
+
+        wanted_nfts.push(token.clone());
+
+        if peer
+            != Cw721Contract(token.collection.clone())
+                .owner_of(&deps.querier, token.token_id.to_string(), false)?
+                .owner
+        {
+            return Err(ContractError::UnauthorizedPeer {
+                collection: token.collection.to_string(),
+                token_id: token.token_id,
+                peer: peer.into_string(),
+            });
+        }
+    }
+
     // check if the sender is the owner of the tokens
-    // TODO: Consider a different order of checks: Now, you might get a not approved error, after which you approved, but actually there is another error, like the peer is not the right owner.
-    //          Then you've approved the contract but no offer has been made, which feels a bit unsafe.
     for token in offered_tokens {
         // Verify token collection addr
         let collection = api.addr_validate(&token.collection)?;
@@ -97,38 +127,6 @@ pub fn execute_create_offer(
             }
         }
     }
-
-    // check if the peer is the owner of the requested tokens
-    for token in wanted_tokens {
-        // Verify token collection addr
-        let collection = api.addr_validate(&token.collection)?;
-
-        let token = Token {
-            collection,
-            token_id: token.token_id,
-        };
-
-        wanted_nfts.push(token.clone());
-
-        if peer
-            != Cw721Contract(token.collection.clone())
-                .owner_of(&deps.querier, token.token_id.to_string(), false)?
-                .owner
-        {
-            return Err(ContractError::UnauthorizedPeer {
-                collection: token.collection.to_string(),
-                token_id: token.token_id,
-                peer: peer.into_string(),
-            });
-        }
-    }
-    let params = SUDO_PARAMS.load(deps.storage)?;
-    // check if the expiry date is valid
-    let expires =
-        expires_at.unwrap_or_else(|| env.block.time.plus_seconds(params.offer_expiry.min + 1));
-    params
-        .offer_expiry
-        .is_valid(&env.block, env.block.time, expires)?;
 
     // create and save offer
     let offer = Offer {
